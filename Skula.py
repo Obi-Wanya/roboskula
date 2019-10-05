feb6270def8b1f0bfa33013959883010016317aa
Obi-Wanya <56207461+Obi-Wanya@users.noreply.github.com>
2019-10-06 02:18:15 +0300

    Update Skula.py

--- Skula.py
+++ Skula.py
@@ -39,7 +39,8 @@ async def on_message(message):
         await message.channel.send(kv)
         await message.channel.send('Сейчас '+str(now))
 
-Bot.run('NjMwMDcwMzI1NzUyNzU4Mjgz.XZkdkg.Mg-f2JrEN2h7UxPbPjCJGN2sxKM') #токен бота
+token = os.environ.get('BOT_TOKEN')
+Bot.run(str(token)) #токен бота
 
 #Ссылка для добавление бота на дс сервер:
 #https://discordapp.com/oauth2/authorize?client_id=630070325752758283&scope=bot&permissions=8
